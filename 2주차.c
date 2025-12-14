46faa4b3e347adfdb0f06c0570e12202759012fe
chamsuae-hash <chamsuae@gmail.com>
2025-12-14 23:17:58 +0900

    Update 2주차.c

--- 2주차.c
+++ 2주차.c
@@ -6,4 +6,45 @@ int main()
 	for (int i = 0; i < 3; i++) scanf("%d", &array[i]);
 	for (int i = 0; i < 3; i++) printf("%d", array[i]);
 }
+
 */
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int in[101];
+    int out[101];
+    int stock[101];
+    int id;
+    int i;
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        scanf("%d", &in[i]);
+    }
+
+    for (i = 1; i <= n; i++)
+    {
+        scanf("%d", &out[i]);
+    }
+
+    for (i = 1; i <= n; i++)
+    {
+        stock[i] = in[i] - out[i];
+    }
+
+    scanf("%d", &id);
+
+    printf("%d\n", stock[id]);
+
+    for (i = 1; i <= n; i++)
+    {
+        printf("%d ", stock[i]);
+    }
+
+    return 0;
+}
