a91d79afc2a020d0b68b36514935c5dbaf3abae0
chamsuae-hash <chamsuae@gmail.com>
2025-12-14 23:16:14 +0900

    Update 11주차.c

--- 11주차.c
+++ 11주차.c
@@ -1,106 +1,157 @@
-/*
-
 #include <stdio.h>
 #include <string.h>
 
-#define MAX 5
+struct Product {
+    int id;
+    char name[20];
+    int price;
+    int in_qty;
+    int out_qty;
+    int total_sales;
+};
+
+struct Product list[5];
+int count = 0;
 
 int main() {
-    int stock[MAX] = { 0 };
-    int sold[MAX] = { 0 };
-    char names[MAX][50];
-    int choice;
-
-    for (int i = 0; i < MAX; i++) {
-        sprintf(names[i], "상품%d", i + 1);
-    }
+    int menu;
+    int i;
+    int find_index;
+    int input_id;
+    int input_qty;
+    FILE* fp;
 
     while (1) {
-        printf("\n===== 쇼핑몰 재고 관리 =====\n");
-        printf("1. 입고\n");
-        printf("2. 판매\n");
-        printf("3. 상품현황\n");
-        printf("4. 상품명 입력\n");
-        printf("5. 종료\n");
-        printf("메뉴 선택: ");
-        scanf("%d", &choice);
+        printf("\n원하는 메뉴를 선택하세요.(1. 입고, 2. 판매, 3. 개별현황, 4. 전체현황, 5. 종료, 6. 저장, 7. 불러오기)\n");
+        scanf("%d", &menu);
 
-        if (choice == 1) {
-            int id, amount;
-            printf("입고할 상품 ID(1~5): ");
-            scanf("%d", &id);
-            printf("입고 수량: ");
-            scanf("%d", &amount);
-            if (id >= 1 && id <= MAX) {
-                stock[id - 1] += amount;
-                printf("%s 입고 완료! 현재 재고: %d\n", names[id - 1], stock[id - 1]);
+        if (menu == 1) {
+            printf("상품ID : ");
+            scanf("%d", &input_id);
+
+            find_index = -1;
+            for (i = 0; i < count; i++) {
+                if (list[i].id == input_id) {
+                    find_index = i;
+                    break;
+                }
+            }
+
+            if (find_index != -1) {
+                printf("입고량 : ");
+                scanf("%d", &input_qty);
+                list[find_index].in_qty += input_qty;
             }
             else {
-                printf("잘못된 상품 ID!\n");
-            }
-        }
-        else if (choice == 2) {
-            int id, amount;
-            printf("판매할 상품 ID(1~5): ");
-            scanf("%d", &id);
-            printf("판매 수량: ");
-            scanf("%d", &amount);
-            if (id >= 1 && id <= MAX) {
-                if (stock[id - 1] >= amount) {
-                    stock[id - 1] -= amount;
-                    sold[id - 1] += amount;
-                    printf("%s 판매 완료! 남은 재고: %d\n", names[id - 1], stock[id - 1]);
+                if (count >= 5) {
+                    printf("상품 갯수 초과 시 메시지 출력\n");
                 }
                 else {
-                    printf("재고 부족! 현재 재고: %d\n", stock[id - 1]);
+                    list[count].id = input_id;
+                    printf("상품명 : ");
+                    scanf("%s", list[count].name);
+                    printf("입고량 : ");
+                    scanf("%d", &list[count].in_qty);
+                    printf("판매가격 : ");
+                    scanf("%d", &list[count].price);
+                    list[count].out_qty = 0;
+                    list[count].total_sales = 0;
+                    count++;
                 }
             }
-            else {
-                printf("잘못된 상품 ID!\n");
-            }
         }
-        else if (choice == 3) {
-            int totalStock = 0, totalSold = 0;
-            for (int i = 0; i < MAX; i++) {
-                totalStock += stock[i];
-                totalSold += sold[i];
-            }
-            printf("\n--- 상품현황 ---\n");
-            printf("재고수량: ");
-            for (int i = 0; i < MAX; i++) {
-                printf("%d ", stock[i]);
-            }
-            printf("\n총 판매량: %d\n", totalSold);
-            if (totalStock + totalSold > 0) {
-                double rate = (double)totalSold / (totalStock + totalSold) * 100;
-                printf("판매율: %.2f%%\n", rate);
+        else if (menu == 2) {
+            printf("상품ID : ");
+            scanf("%d", &input_id);
+
+            find_index = -1;
+            for (i = 0; i < count; i++) {
+                if (list[i].id == input_id) {
+                    find_index = i;
+                    break;
+                }
             }
 
-            int maxIdx = 0, minIdx = 0;
-            for (int i = 1; i < MAX; i++) {
-                if (sold[i] > sold[maxIdx]) maxIdx = i;
-                if (sold[i] < sold[minIdx]) minIdx = i;
+            if (find_index == -1) {
+                printf("오류메시지 출력\n");
             }
-            printf("가장 많이 판매된 상품: ID %d, %s, 판매량 %d\n",
-                maxIdx + 1, names[maxIdx], sold[maxIdx]);
-            printf("가장 적게 판매된 상품: ID %d, %s, 판매량 %d\n",
-                minIdx + 1, names[minIdx], sold[minIdx]);
-        }
-        else if (choice == 4) {
-            for (int i = 0; i < MAX; i++) {
-                printf("ID %d 상품명 입력: ", i + 1);
-                scanf("%s", names[i]);
+            else {
+                printf("판매량 : ");
+                scanf("%d", &input_qty);
+                list[find_index].out_qty += input_qty;
+                list[find_index].total_sales += (input_qty * list[find_index].price);
             }
         }
-        else if (choice == 5) {
-            printf("프로그램 종료!\n");
+        else if (menu == 3) {
+            printf("상품ID : ");
+            scanf("%d", &input_id);
+
+            find_index = -1;
+            for (i = 0; i < count; i++) {
+                if (list[i].id == input_id) {
+                    find_index = i;
+                    break;
+                }
+            }
+
+            if (find_index != -1) {
+                printf("ID: %d, 이름: %s, 가격: %d, 입고: %d, 판매: %d, 총매출: %d\n",
+                    list[find_index].id, list[find_index].name, list[find_index].price,
+                    list[find_index].in_qty, list[find_index].out_qty, list[find_index].total_sales);
+            }
+            else {
+                printf("상품이 없습니다.\n");
+            }
+        }
+        else if (menu == 4) {
+            printf("전체 상품 정보 출력\n");
+            for (i = 0; i < count; i++) {
+                int stock = list[i].in_qty - list[i].out_qty;
+                double rate = 0.0;
+                if (list[i].in_qty > 0) {
+                    rate = (double)list[i].out_qty / list[i].in_qty * 100;
+                }
+                printf("ID: %d, 이름: %s, 재고: %d, 판매율: %.1lf%%\n",
+                    list[i].id, list[i].name, stock, rate);
+            }
+        }
+        else if (menu == 5) {
+            printf("프로그램을 종료합니다.\n");
             break;
         }
-        else {
-            printf("잘못된 메뉴 선택!\n");
+        else if (menu == 6) {
+            fp = fopen("data.txt", "w");
+            if (fp == NULL) {
+                printf("파일 저장 실패\n");
+            }
+            else {
+                fprintf(fp, "%d\n", count);
+                for (i = 0; i < count; i++) {
+                    fprintf(fp, "%d %s %d %d %d %d\n",
+                        list[i].id, list[i].name, list[i].price,
+                        list[i].in_qty, list[i].out_qty, list[i].total_sales);
+                }
+                fclose(fp);
+                printf("상품정보 저장 완료\n");
+            }
+        }
+        else if (menu == 7) {
+            fp = fopen("data.txt", "r");
+            if (fp == NULL) {
+                printf("파일이 없습니다.\n");
+            }
+            else {
+                fscanf(fp, "%d", &count);
+                for (i = 0; i < count; i++) {
+                    fscanf(fp, "%d %s %d %d %d %d",
+                        &list[i].id, list[i].name, &list[i].price,
+                        &list[i].in_qty, &list[i].out_qty, &list[i].total_sales);
+                }
+                fclose(fp);
+                printf("상품정보 불러오기 완료\n");
+            }
         }
     }
+
     return 0;
 }
-
-*/
